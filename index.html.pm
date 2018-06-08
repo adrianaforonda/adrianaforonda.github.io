e1f67d18ccb17a00446232464b06721aa3b0b196
Mauricio Foronda <josemauricioforonda@gmail.com>
2018-06-07 22:23:12 -0400

    CV

---
+++ index.html.pm
@@ -0,0 +1,168 @@
+#lang pollen
+
+◊title{Adriana Foronda}
+
+◊section{
+
+◊em{Mobile}: +591 68147994 
+◊em{Email}: adrianabelenforonda@gmail.com
+◊em{Address}: Abdón Saavedra between Aspiazu and Chaco N° 43 
+La Paz, Plurinational State of Bolivia
+
+}
+
+◊section{
+
+◊h2{Education}
+
+◊h3{Masters, Diplomacy and International Relations}
+Thesis defense scheduled for May-2018
+Diplomatic Academy of Bolivia
+Ministry of Foreign Affairs, Plurinational State of Bolivia ◊marginnote{◊link["http://www.academiadiplomatica.gob.bo/"]{academiadiplomatica.gob.bo}}
+
+◊h3{Masters, Critical Studies of Development}
+2013- 2014
+Ciencias del Desarrollo - Universidad Mayor de San Andrés (CIDES - UMSA) ◊marginnote{◊link["http://www.cides.edu.bo"]{cides.edu.bo}}
+
+◊h3{Bachelor, Economics}
+2005-2009
+Universidad Mayor de San Andrés (UMSA) ◊marginnote{◊link["http://www.umsa.bo"]{umsa.bo}}
+
+}
+
+◊section{
+
+◊h2{Courses}
+
+◊h3{World Trade Organization (WTO)}
+E-Learning Hub◊marginnote{◊link["http://ecampus.wto.org"]{ecampus.wto.org}}
+2017 - Present
+
+◊h3{Asociación Latinoamericana de Integración (ALADI)}
+Centro Virtual de Formación◊marginnote{◊link["http://campus.aladi-cvf.org"]{campus.aladi-cvf.org}}
+2017 - Present
+
+}
+
+◊section{
+
+◊h2{Work Experience}
+
+◊h3{Research Officer, InternetBolivia.org}
+◊marginnote{◊link["https://internetbolivia.org"]{internetbolivia.org}}
+La Paz, 1/2018 – Present
+* Responsible for research projects studying the relationships between the internet, the economy, trade and development
+* Responsible for gender-related initiatives
+
+◊h3{Graduate Research Student, Academia Diplomática Plurinacional de Bolivia}
+◊marginnote{◊link["http://www.academiadiplomatica.gob.bo/"]{academiadiplomatica.gob.bo}}
+La Paz, 1/2018 – Present
+
+◊h3{Trade Negotiations Unit Support (ad honorem), Ministry of Foreign Affairs, Plurinational State of Bolivia}
+◊marginnote{◊link["http://www.cancilleria.gob.bo"]{cancilleria.gob.bo}}
+La Paz,  6/2017 – 12/2017
+* Logistic support for the Trade Facilitation Agreement - WTO
+* Logistic support for the Bolivian Trade Policy Review - WTO
+* Technical support for the Trade Policy Review - WTO
+* Economic analysis of bilateral relations between Bolivia and other countries
+
+◊h3{Online course responsible, Rural Development for South America Institute (IPDRS)}
+◊marginnote{◊link["http://www.ipdrs.org/"]{ipdrs.org}}
+La Paz, 10/2015 – 12/2015
+Course: Climate Risk, adaptation and mitigation for Security Food
+* Online course implementation
+* Teacher-student logistics
+* Preparation of documentation and related articles
+* Logistics of related physical events
+
+◊h3{Office support (ad honorem), Embassy of the Plurinational State of Bolivia in the Republic Federal of Germany}
+◊marginnote{◊link["http://www.bolivia.de"]{bolivia.de}}
+Berlin, 7/2016 – 8/2015 
+* Support for consular services
+* Organization of the official celebration for the 190th anniversary of Bolivian independence
+* Database upgrade
+
+◊h3{Project Manager, Gobierno Autónomo Municipal de La Paz, La Paz}
+◊marginnote{◊link["http://www.lapazbus.bo"]{lapazbus.bo}}
+La Paz, 9/2014 - 12/2014
+* Responsible for the economic planning and development  of an important public transportation project
+
+◊h3{Business Development Responsible, Asociación Cuna (Inter-American Development Bank funding)}
+La Paz, 1/2013 - 9/2013
+* Generate market opportunities for 10 Small and Medium Enterprises, mainly craftswomen
+* Product quality assistance
+* Manage relations with the Ministry of Cultures and Tourism
+
+◊h3{Responsible of Development Projects, Gobierno Autónomo Departamental de Tarija (La Paz representation)}
+La Paz, 7/2012- 12/ 2012
+* Manage food security projects
+* Create projects related to productive development
+* Representative at the committee against racism and all forms of discrimination
+
+◊h3{FairTrade Area Responsible (ad honorem), Promueve Bolivia (Ministry of Plural Economy and Productive Development)}
+La Paz, 5/2012- 6/2012
+* Logistic support at events and meetings
+
+◊h3{Trade Coordinator, SURI}
+La Paz, 10/2011 - 4/2012
+
+◊h3{Project Manager, Interdisciplina para el Desarrollo funded by the World Bank}
+La Paz, 7/2012 - 12/2012
+* Evaluate projects for the Rural Alliances Project - World Bank
+* Visit coffee and cacao cooperatives
+
+}
+
+◊section{
+
+◊h2{Language Skills}
+
+◊ul{
+◊li{Spanish, First Language}
+◊li{English, Advanced}
+◊li{Italian, Intermedia}
+◊li{French, Intermediate}
+◊li{German, Basic}
+◊li{Aymara, Basic}
+}
+
+}
+
+◊section{
+
+◊h2{Computer Skills}
+
+◊ul{
+◊li{Microsoft Word}
+◊li{Microsoft Excel}
+◊li{Microsoft PowerPoint}
+◊li{SPSS}
+◊li{E-views}
+◊li{Google Docs}
+◊li{Internet}
+}
+
+}
+
+◊section{
+
+◊h2{Events}
+
+◊h3{Trade and Sustainable Development Symposium}
+Organized by International Centre for Trade and Sustainable Development (ICTSD)
+Buenos Aires, 2015
+
+◊h3{Arbeitsgemeinschaft Österreichische Lateinamerikaforschung}
+Organized by the Latin America Association in Austria
+Strobl-Austria, 2015
+
+}
+
+◊section{
+
+◊h2{Publications}
+
+◊h3{Análisis del Comercio Justo en Bolivia: caso sector cafetalero (2000 - 2010)}
+Centro de Investigaciones Sociales (La Paz, Bolivia). (2016). Bolivia : escenarios en transformación: Artículos sobre política, cultura y economía.
+
+}
